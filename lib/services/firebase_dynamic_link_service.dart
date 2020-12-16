5e3bfd4675ed9df1d928024cd435b3110b9aa4c4
ro.stradamus <rolee0429@gmail.com>
2020-12-16 15:10:08 -0800

    JMO-18: Implement Firebase

--- lib/services/firebase_dynamic_link_service.dart
+++ lib/services/firebase_dynamic_link_service.dart
@@ -2,27 +2,80 @@ import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:jmorder_app/app.dart';
 
-class DynamicLinkService {
-  Future<void> retrieveDynamicLink(BuildContext context) async {
-    try {
-      final PendingDynamicLinkData data =
-          await FirebaseDynamicLinks.instance.getInitialLink();
-      final Uri deepLink = data?.link;
+class FirebaseDynamicService {
+  // Future<void> retrieveDynamicLink(BuildContext context) async {
+  //   try {
+  //     final PendingDynamicLinkData data =
+  //         await FirebaseDynamicLinks.instance.getInitialLink();
+  //     final Uri deepLink = data?.link;
 
-      if (deepLink != null) {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => App()));
-      }
+  //     if (deepLink != null) {
+  //       Navigator.of(context)
+  //           .push(MaterialPageRoute(builder: (context) => App()));
+  //     }
 
-      FirebaseDynamicLinks.instance.onLink(
-          onSuccess: (PendingDynamicLinkData dynamicLink) async {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => App()));
-      });
-    } catch (e) {
-      print(e.toString());
-    }
-  }
+  //     FirebaseDynamicLinks.instance.onLink(
+  //         onSuccess: (PendingDynamicLinkData dynamicLink) async {
+  //       Navigator.of(context)
+  //           .push(MaterialPageRoute(builder: (context) => App()));
+  //     });
+  //   } catch (e) {
+  //     print(e.toString());
+  //   }
+  // }
 
-  ///createDynamicLink()
+  // static void initDynamicLinks() async {
+  //   final PendingDynamicLinkData data =
+  //       await FirebaseDynamicLinks.instance.getInitialLink();
+
+  //   _handleDynamicLink(data);
+
+  //   FirebaseDynamicLinks.instance.onLink(
+  //       onSuccess: (PendingDynamicLinkData dynamicLink) async {
+  //     _handleDynamicLink(dynamicLink);
+  //   }, onError: (OnLinkErrorException e) async {
+  //     print('onLinkError');
+  //     print(e.message);
+  //   });
+  // }
+
+  // static _handleDynamicLink(PendingDynamicLinkData data) async {
+  //   final Uri deepLink = data?.link;
+
+  //   if (deepLink == null) {
+  //     return;
+  //   }
+  //   if (deepLink.pathSegments.contains('refer')) {
+  //     var title = deepLink.queryParameters['code'];
+  //     if (title != null) {
+  //       print("refercode=$title");
+  //     }
+  //   }
+  // }
+
+  // Future handleDynamicLinks() async {
+  //   // 1. Get the initial dynamic link if the app is opened with a dynamic link
+  //   final PendingDynamicLinkData data =
+  //       await FirebaseDynamicLinks.instance.getInitialLink();
+
+  //   // 2. handle link that has been retrieved
+  //   _handleDeepLink(data);
+
+  //   // 3. Register a link callback to fire if the app is opened up from the background
+  //   // using a dynamic link.
+  //   FirebaseDynamicLinks.instance.onLink(
+  //       onSuccess: (PendingDynamicLinkData dynamicLink) async {
+  //     // 3a. handle link that has been retrieved
+  //     _handleDeepLink(dynamicLink);
+  //   }, onError: (OnLinkErrorException e) async {
+  //     print('Link Failed: ${e.message}');
+  //   });
+  // }
+
+  // void _handleDeepLink(PendingDynamicLinkData data) {
+  //   final Uri deepLink = data?.link;
+  //   if (deepLink != null) {
+  //     print('_handleDeepLink | deeplink: $deepLink');
+  //   }
+  // }
 }
--- lib/utils/service_locator.dart
+++ lib/utils/service_locator.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:jmorder_app/services/auth_service.dart';
 import 'package:get_it/get_it.dart';
+import 'package:jmorder_app/services/firebase_dynamic_link_service.dart';
 import 'package:jmorder_app/services/jmo_api_service.dart';
 import 'package:jmorder_app/services/clients_service.dart';
 import 'package:jmorder_app/services/integration_service.dart';
@@ -11,6 +13,8 @@ class ServiceLocator {
         () async => JmoApiService()..init());
     GetIt.I
         .registerSingletonAsync<AuthService>(() async => AuthService()..init());
+    // GetIt.I.registerLazySingleton<FirebaseDynamicService>(
+    //     () => FirebaseDynamicService());
     GetIt.I.registerSingleton<VerificationService>(VerificationService());
     GetIt.I.registerSingleton<ClientsService>(ClientsService());
     GetIt.I.registerSingleton<OrdersService>(OrdersService());
